0c685564cb6e70a3179cad26cb2c9ba66ae02fdd
jwilson10 <jessica.a.wilson99@gmail.com>
2023-01-09 17:17:42 -0500

    Completed Project

--- interactive-rating-component-main/script.js
+++ interactive-rating-component-main/script.js
@@ -1,8 +1,12 @@
-const ratingCard = document.querySelector("#ratingCard");
-const thankYouCard = document.querySelector("#thankYouCard");
+const page = document.querySelector(".page");
+const ratingCard = page.querySelector("#ratingCard");
+const thankYouCard = page.querySelector("#thankYouCard");
 const form = ratingCard.querySelector("#rateUs");
 const buttonDiv = form.querySelector("#buttons");
 const buttons = buttonDiv.querySelectorAll(".rating");
+const ratingSentence = thankYouCard.querySelector(".ratingSentence");
+
+let currentRating = 0;
 
 function buttonClick(){
     const currentSelected = buttonDiv.querySelector(".selected");
@@ -12,9 +16,20 @@ function buttonClick(){
     if(!currentSelected || currentSelected.id != this.id){
         this.classList.add("selected");
     }
+    currentRating = this.value;
 }
 
 
 for (var i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener('click', buttonClick);
 }
+
+form.addEventListener("submit", (evt) =>{
+    evt.preventDefault();
+
+    if(currentRating != 0){
+        ratingCard.style.display = "none";
+        thankYouCard.style.display = "block";
+        ratingSentence.innerText = `You selected ${currentRating} out of 5`
+    }
+});
