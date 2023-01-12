9bd783933b6622a6eabd2c7e6b58ab22a87e911a
jwilson10 <jessica.a.wilson99@gmail.com>
2023-01-12 16:20:48 -0500

    Roughed out components

---
+++ _portfolio_site/portfolio/src/components/HelloWorld.js
@@ -0,0 +1,12 @@
+
+
+function HelloWorld(){
+    return(
+    <header>
+        <div className="heading">
+            <div className="introduction"><h1>Hello World. I'm Jessica. Get to know a bit more about me.</h1></div>
+        </div>
+    </header>)
+}
+
+export default HelloWorld;
